2036713a9219ff3bc18f85a4f48e8cc37b61d8ea
AristoSci <mattwilroe@gmail.com>
2021-07-30 16:02:13 -0400

    Update response for testing pipeline

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const HOST = process.env.HOST;
 // App
 const app = express();
 app.get('/', (req, res) => { 
-  res.send('Hello World Again AGAIN'); 
+  res.send('Hello World'); 
 });
 
 app.listen(PORT, HOST);
